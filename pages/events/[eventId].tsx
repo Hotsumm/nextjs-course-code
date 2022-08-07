0300acfc5d4a92d22894c8c700af65ebc76cab1b
Yeolha Sung <tjddufgk@gmail.com>
2022-08-08 02:11:39 +0900

    feat: add event detail components

--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -1,7 +1,32 @@
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
+import {
+  EventSummary,
+  EventLogistics,
+  EventContent,
+} from '../../src/components/eventDetail';
+
 export default function EventDetail() {
+  const router = useRouter();
+
+  const { eventId } = router.query as { eventId: string };
+  const event = getEventById(eventId);
+
+  if (!event) return <p>No event found!</p>;
+
   return (
-    <div>
-      <h1>Event Detail Page</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
---
+++ src/components/eventDetail/EventLogistics.tsx
@@ -0,0 +1,42 @@
+import { AddressIcon, DateIcon } from '../icons';
+import LogisticsItem from './LogisticsItem';
+import classes from './EventLogistics.module.css';
+
+type EventLogisticsProps = {
+  date: string;
+  address: string;
+  image: string;
+  imageAlt: string;
+};
+
+function EventLogistics({
+  date,
+  address,
+  image,
+  imageAlt,
+}: EventLogisticsProps) {
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const addressText = address.replace(', ', '\n');
+
+  return (
+    <section className={classes.logistics}>
+      <div className={classes.image}>
+        <img src={`/${image}`} alt={imageAlt} />
+      </div>
+      <ul className={classes.list}>
+        <LogisticsItem icon={DateIcon}>
+          <time>{humanReadableDate}</time>
+        </LogisticsItem>
+        <LogisticsItem icon={AddressIcon}>
+          <address>{addressText}</address>
+        </LogisticsItem>
+      </ul>
+    </section>
+  );
+}
+
+export default EventLogistics;
