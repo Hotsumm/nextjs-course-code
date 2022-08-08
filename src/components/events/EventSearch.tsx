86bcd0a50a05be00805bd688290a6cd62fe6afba
Yeolha Sung <tjddufgk@gmail.com>
2022-08-08 21:03:37 +0900

    fix: change search select month options

--- src/components/events/EventSearch.tsx
+++ src/components/events/EventSearch.tsx
@@ -32,16 +32,16 @@ function EventsSearch({ onSearch }: EventsSearchProps) {
           <select id="month" ref={monthRef}>
             <option value="1">January</option>
             <option value="2">February</option>
-            <option value="2">March</option>
-            <option value="2">April</option>
-            <option value="2">May</option>
-            <option value="2">June</option>
-            <option value="2">July</option>
-            <option value="2">August</option>
-            <option value="2">September</option>
-            <option value="2">October</option>
-            <option value="2">November</option>
-            <option value="2">December</option>
+            <option value="3">March</option>
+            <option value="4">April</option>
+            <option value="5">May</option>
+            <option value="6">June</option>
+            <option value="7">July</option>
+            <option value="8">August</option>
+            <option value="9">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
           </select>
         </div>
       </div>
