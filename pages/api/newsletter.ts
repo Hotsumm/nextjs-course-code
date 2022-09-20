9e51e656e12ffae3505579d710c2d4f990b23bd2
Yeolha Sung <tjddufgk@gmail.com>
2022-09-20 18:12:21 +0900

    feat: add newletter api connection mongodb

--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -1,9 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { MongoClient } from 'mongodb';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const URL =
+  'mongodb+srv://hotsumm:SP5KlctDgWmExOzR@cluster0.gh1i0kd.mongodb.net/?retryWrites=true&w=majority';
+const DB_NAME = 'newletter';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === 'POST') {
     const { email }: any = req.body;
-    console.log(email);
+
+    const client = new MongoClient(URL);
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(DB_NAME);
+    const collection = db.collection('emails');
+
+    try {
+      await collection.insertOne({ email });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      client.close();
+    }
 
     res.status(201).json({ message: '등록 성공' });
   }
